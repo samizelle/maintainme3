d8e6f33f88e8af63f9781017cec9d8862810cb4f
Stephanie Mizelle <samizelle@gmail.com>
2018-12-02 20:27:23 -0500

    commit Index, Create views

---
+++ MaintainMe.Models/CarCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintainMe.Models
+{
+    public class CarCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage ="Please enter at least two characters")]
+        [MaxLength(25, ErrorMessage ="Make can be no longer than 25 characters")]
+        public string Make { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least two characters")]
+        [MaxLength(25, ErrorMessage = "Model can be no longer than 25 characters")]
+        public string Model { get; set; }
+    }
+}
---
+++ MaintainMe.WebMVC/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MaintainMe.Models;
+
+namespace MaintainMe.WebMVC.Controllers
+{
+    [Authorize]
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Index()
+        {
+            var model = new CarListItem[0];
+            return View(model);
+        }
+
+        // GET: Car Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
