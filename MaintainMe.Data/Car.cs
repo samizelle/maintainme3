e4536f0a0314701fd5017ac683c2d4ed41fe0cc8
Stephanie Mizelle <samizelle@gmail.com>
2018-12-02 20:07:59 -0500

    commit data tables

---
+++ MaintainMe.Data/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintainMe.Data
+{
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+        public int OwnerId { get; set; }
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+    }
+}
---
+++ MaintainMe.Data/ServiceEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintainMe.Data
+{
+    public class ServiceEvent
+    {
+        [Key]
+        public int ServiceId { get; set; }
+        public int CarId { get; set; }
+        [Required]
+        public int Mileage { get; set; }
+        [Required]
+        public DateTime ServiceDate { get; set; }
+    }
+}
